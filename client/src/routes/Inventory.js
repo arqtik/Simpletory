1ffc9d71f99e21d27984fb5b026176f51827eef8
arqtik <23316648+arqtik@users.noreply.github.com>
2023-10-24 16:02:10 +0200

    Update Inventory.js
    Now lists items from api

--- client/src/routes/Inventory.js
+++ client/src/routes/Inventory.js
@@ -6,15 +6,36 @@ import {
 
 export async function loader() {
     const items = await getItems();
-    return { items };
+    return items;
 }
 
 export default function Inventory(){
     const items = useLoaderData();
 
     return(
-        <div>
-            Inventory Not Implemented
-        </div>
+        <table className="table table">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Mass</th>
+                    <th scope="col">#</th>
+                    <th scope="col">Location</th>
+                    <th scope="col">View</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    items.map((item) => (
+                            <tr key={item.id}>
+                                <td>{item.name}</td>
+                                <td>{item.mass}{item.unit}</td>
+                                <td>{item.count}</td>
+                                <td>{item.location}</td>
+                                <td><Link to={`item/${item.id}`} className="btn btn-primary btn-sm">View</Link></td>
+                            </tr>
+                    ))
+                }
+            </tbody>
+        </table>
     );
 }
